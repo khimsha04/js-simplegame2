c4e3c6fb18863f65009aeb6efbef586d789a994f
uknown-me <64226929+uknown-me@users.noreply.github.com>
2022-08-01 19:46:42 +0400

    add more js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,90 @@
-const board = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-];
-let elementTd = document.querySelectorAll("td");
-const button = document.querySelector(".button");
-let getRandomNumber = function () {
-  let valueRandomNumber = board[Math.floor(Math.random() * board.length)];
-};
+let myArr = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ],
+  divOfBlocks = "",
+  section = document.querySelector("section"),
+  button = document.createElement("button"),
+  table = document.createElement("table");
 
-for (let i = 0; i < board.length; i++) {
-  for (let j = 0; j < board.length; j++) {
-    const element = elementTd[i][j];
-    const randomNumber = getRandomNumber();
-  }
+for (let i = 0; i < myArr.length; i++) {
+  divOfBlocks += `
+     <tr>
+     <td></td>
+     <td></td>
+     <td></td>
+     <td></td>
+     <td></td>
+     </tr>`;
 }
 
-button.addEventListener("click", function () {
-  board[0][1] = 1;
-  board[1][2] = 1;
-  board[2][3] = 1;
-  console.log(index);
-  if (board === 1) {
+button.innerHTML = "START";
+
+table.innerHTML = divOfBlocks;
+section.appendChild(table);
+section.appendChild(button);
+const arrCount = myArr.reduce(
+  (currentCount, row) => currentCount + row.length,
+  0
+);
+
+let blocks = document.getElementsByTagName("td");
+
+function randomNumGen() {
+  for (let i = 0; i < 3; i++) {
+    let randomNum = Math.floor(Math.random() * arrCount);
+    if (randomNum < 5) {
+      myArr[0][randomNum] = 1;
+    } else if (randomNum >= 5 && randomNum < 10) {
+      randomNum -= 5;
+      myArr[1][randomNum] = 1;
+    } else if (randomNum >= 10 && randomNum < 15) {
+      randomNum -= 10;
+      myArr[2][randomNum] = 1;
+    } else if (randomNum >= 15 && randomNum < 20) {
+      randomNum -= 15;
+      myArr[3][randomNum] = 1;
+    } else {
+      randomNum -= 20;
+      myArr[4][randomNum] = 1;
+    }
   }
-});
+  yellowBlocks();
+  console.log("event happened");
+  button.removeEventListener("click", randomNumGen);
+}
+button.addEventListener("click", randomNumGen);
+// button.addEventListener("click", function () {
+//   randomNumGen();
+//   if (this.click === myArr[i][a]) {
+//     blocks[myNum].style.backgroundColor = "green";
+//   } else {
+//     blocks[myNum].style.backgroundColor = "red";
+//   }
+// });
+
+function yellowBlocks() {
+  let myNum = 0;
+  for (let i = 0; i < myArr.length; i++) {
+    for (let a = 0; a < myArr[i].length; a++) {
+      if (myArr[i][a] === 1) {
+        blocks[myNum].style.backgroundColor = "yellow";
+      }
+      myNum++;
+    }
+  }
+  myNum = 0;
+  setTimeout(() => {
+    for (let i = 0; i < myArr.length; i++) {
+      for (let a = 0; a < myArr[i].length; a++) {
+        if (myArr[i][a] === 1) {
+          blocks[myNum].style.backgroundColor = "white";
+        }
+        myNum++;
+      }
+    }
+  }, "1000");
+}
